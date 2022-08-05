d6dec817cd6e1890b6ea3834bc77618f6be0b98e
jwrick816 <86348963+jwrick816@users.noreply.github.com>
2022-08-05 10:07:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,6 @@
 #!bin/bash
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-
-apt-get -y install git docker-compose
-systemctl restart docker.service
 
 git clone https://github.com/GreenFrogSB/LMDS.git
 cd LMDS/
-sh deploy.sh
+./deploy.sh
+exit
